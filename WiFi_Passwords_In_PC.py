25e533bbd89caaa26d266bff32bb0b62bd7e7998
Subhajit Majee <94911109+subhajitmajee@users.noreply.github.com>
2023-11-11 13:30:46 +0530

    Update WiFi_Passwords_In_PC.py

--- WiFi_Passwords_In_PC.py
+++ WiFi_Passwords_In_PC.py
@@ -1,18 +1,69 @@
+# importing subprocess
 import subprocess
 
-data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
+# getting meta data
+meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
 
-profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+# decoding meta data
+data = meta_data.decode('utf-8', errors ="backslashreplace")
 
+# splitting data by line by line
+data = data.split('\n')
+
+# creating a list of profiles
+profiles = []
+
+# traverse the data
+for i in data:
+	
+	# find "All User Profile" in each item
+	if "All User Profile" in i :
+		
+		# if found
+		# split the item 
+		i = i.split(":")
+		
+		# item at index 1 will be the wifi name
+		i = i[1]
+		
+		# formatting the name
+		# first and last character is use less
+		i = i[1:-1]
+		
+		# appending the wifi name in the list
+		profiles.append(i)
+		
+
+# printing heading	 
+print("{:<30}| {:<}".format("Wi-Fi Name", "Password"))
+print("----------------------------------------------")
+
+# traversing the profiles	 
 for i in profiles:
-    try:
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8', errors="backslashreplace").split('\n')
-        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-        try:
-            print ("{:<30}|  {:<}".format(i, results[0]))
-        except IndexError:
-            print ("{:<30}|  {:<}".format(i, ""))
-    except subprocess.CalledProcessError:
-        print ("{:<30}|  {:<}".format(i, "ENCODING ERROR"))
-
-input("")
+	
+	# try catch block begins
+	# try block
+	try:
+		# getting meta data with password using wifi name
+		results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key = clear'])
+		
+		# decoding and splitting data line by line
+		results = results.decode('utf-8', errors ="backslashreplace")
+		results = results.split('\n')
+		
+		# finding password from the result list
+		results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+		
+		# if there is password it will print the pass word
+		try:
+			print("{:<30}| {:<}".format(i, results[0]))
+		
+		# else it will print blank in front of pass word
+		except IndexError:
+			print("{:<30}| {:<}".format(i, ""))
+			
+	
+			
+	# called when this process get failed
+	except subprocess.CalledProcessError:
+		print("Encoding Error Occurred")
